f6cb1e452f2a86be43da9fd67cbe9a4041179f84
AurSoft <jonky095@gmail.com>
2021-06-25 01:13:26 +0200

    Added UI tests for Toast messages.

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,18 +1,21 @@
 package com.udacity.project4
 
 import android.app.Application
+import android.view.WindowManager.LayoutParams.*
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.Root
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.ReminderDescriptionViewModel
 import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
@@ -21,6 +24,8 @@ import com.udacity.project4.util.DataBindingIdlingResource
 import com.udacity.project4.util.monitorActivity
 import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -31,6 +36,7 @@ import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.get
+import java.util.regex.Matcher
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -54,6 +60,12 @@ class RemindersActivityTest :
                 RemindersListViewModel(
                     appContext,
                     get() as ReminderDataSource
+                )
+            }
+            viewModel {
+                ReminderDescriptionViewModel(
+                        appContext,
+                        get() as ReminderDataSource
                 )
             }
             single {
@@ -116,4 +128,97 @@ class RemindersActivityTest :
         // When using ActivityScenario.launch, always call close()
         activityScenario.close()
     }
+
+    @Test
+    fun reminderList_saveReminder_showToastMsg()  = runBlocking {
+
+        // Start reminders list screen
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Click on add new reminder
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // On save reminder screen, add the title and description
+        onView(withId(R.id.reminderTitle)).perform(replaceText("Title"))
+        onView(withId(R.id.reminderDescription)).perform(replaceText("Description"))
+        onView(withText("Title")).check(matches(isDisplayed()))
+        onView(withText("Description")).check(matches(isDisplayed()))
+
+        // Then select location
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).perform(longClick())
+        onView(withId(R.id.addMarkerFAB)).perform(click())
+
+        // And then save
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // Toast will display
+        onView(withText(R.string.reminder_saved)).inRoot(ToastMatcher()).check(matches(isDisplayed()))
+
+        // When using ActivityScenario.launch, always call close()
+        activityScenario.close()
+    }
+
+    @Test
+    fun reminderList_seeReminderDescription_showToastMsg() = runBlocking {
+        // Given a reminder
+        val reminderDTO = ReminderDTO("Title1", "Desc1", "Loc1", 1.0, 0.0)
+        repository.saveReminder(reminderDTO)
+
+        // Start reminders list screen
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // Click on add new reminder
+        onView(withId(R.id.reminderCardView)).perform(click())
+
+        // On description screen, check everything is showing
+        onView(withText(R.string.details_screen_title)).check(matches(isDisplayed()))
+        onView(withText("Title1")).check(matches(isDisplayed()))
+        onView(withText("Desc1")).check(matches(isDisplayed()))
+        onView(withText("Loc1")).check(matches(isDisplayed()))
+        onView(withId(R.id.buttonRemoveReminder)).check(matches(isDisplayed()))
+        onView(withId(R.id.imageView)).check(matches(isDisplayed()))
+
+        // Click on remove reminder
+        onView(withId(R.id.buttonRemoveReminder)).perform(click())
+
+        // Toast shows up
+        onView(withText("Reminder removed")).inRoot(ToastMatcher()).check(matches(isDisplayed()))
+
+        // When using ActivityScenario.launch, always call close()
+        activityScenario.close()
+    }
+}
+
+/*
+The reviewer suggested to take a look here to see how to test a Toast message:
+https://stackoverflow.com/questions/28390574/checking-toast-message-in-android-espresso/28606603#28606603
+Here many users suggest to create a custom Matcher.
+This idea works well on Android <= 10. On Android 11, this same solution returns an error:
+https://github.com/android/android-test/issues/803
+ */
+
+private class ToastMatcher : TypeSafeMatcher<Root>() {
+
+    override fun describeTo(description: Description?) {
+        description?.appendText("is toast")
+    }
+
+    override fun matchesSafely(root: Root): Boolean {
+        val type = root.windowLayoutParams.get().type
+        @Suppress("DEPRECATION")
+        if (type == TYPE_TOAST ) {
+            val windowToken = root.decorView.windowToken
+            val appToken = root.decorView.applicationWindowToken
+            if (windowToken === appToken) {
+                // windowToken == appToken means this window isn't contained by any other windows.
+                // if it was a window for an activity, it would have TYPE_BASE_APPLICATION.
+                return true
+            }
+        }
+        return false
+    }
+
 }
